33ef2894d44b460f444e0cc7f6d96defcc9b23ed
Bruno Roberto <roberto.bruno007@gmail.com>
2021-09-14 18:36:22 -0300

    Configuracao do flyway

---
+++ src/main/java/com/communication/schedulingorder/SchedulingOrder.java
@@ -0,0 +1,28 @@
+package com.communication.schedulingorder;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class SchedulingOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private LocalDate dateScheduling;
+
+    private String receiver;
+
+    private String mensage;
+
+    private String typeCommunication;
+
+}
---
+++ src/main/java/com/communication/schedulingorder/SchedulingOrderResource.java
@@ -0,0 +1,12 @@
+package com.communication.schedulingorder;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ordem/v1")
+public class SchedulingOrderResource {
+
+    
+
+}
